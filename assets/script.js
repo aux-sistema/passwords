b24c6d3a2c10135c4ceb7416b09352c4c5409e31
aux-sistema <auxsistemasLdl@gmail.com>
2026-08-28 17:43:18 -0600

    Add user 'Guadalupe' with password to script.js

--- assets/script.js
+++ assets/script.js
@@ -71,6 +71,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { user: "Ian", password: "Ian-005" },
         { user: "Patricia", password: "Patrici4-43" },
         { user: "Lucero", password: "Lucer085$" },
+        { user: "Guadalupe", password: "Lupita$78" },
         
         
     ];
